53be3c0fcfb19c18d68cf2c353c4bc4182c7c8ac
Jason Carr <jasonhcarr@gmail.com>
2016-11-05 19:52:29 -0400

    preparing to merge

--- js/controllers/search.controller.js
+++ js/controllers/search.controller.js
@@ -2,6 +2,9 @@ RecipeBox.controller('SearchController', function ($scope, SearchService, Recipe
   $scope.searchForRecipes = function(input) {
     var results = SearchService.recipes();
     results.then(function(data) {
+      // for () {
+      //   array[i]
+      // }
       console.log(new Recipe(data));
     });
   };
