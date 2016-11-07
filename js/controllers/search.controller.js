2a8717d9bd55b7a2eeb6f2d0d5408f58b7d864ee
Andrew Toups <mistertoups@gmail.com>
2016-11-07 00:06:24 -0500

    Added create recipe button to search view

--- js/controllers/search.controller.js
+++ js/controllers/search.controller.js
@@ -1,4 +1,4 @@
-RecipeBox.controller('SearchController', function ($scope, SearchService, Recipe) {
+RecipeBox.controller('SearchController', function ($scope, SearchService, Recipe, $state) {
   $scope.searchForRecipes = function(input) {
     var searchResults = SearchService.recipes(input);
     // results.then(function(data) {
@@ -13,5 +13,8 @@ RecipeBox.controller('SearchController', function ($scope, SearchService, Recipe
     if (searchResults === []) {
       alert('No Matching Recipes');
     }
+  };
+  $scope.goToCreate = function(){
+    $state.go('recipe-box.box');
   };
 });
